72ff573ab3ae9d088ea13a0e41273747971db59b
Swatlprus <swatlprus@gmail.com>
2022-09-06 16:44:36 +0500

    Add function main() Dry code

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from urllib.parse import urlparse
 from environs import Env
 import argparse
 
-env = Env()
-env.read_env()
-BITLY_TOKEN = env("BITLY_TOKEN")
+BITLY_TOKEN = None
 
 def shorten_link(BITLY_TOKEN, url):
     headers = {'Authorization': f'Bearer {BITLY_TOKEN}'}
@@ -36,14 +34,21 @@ def is_bitlink(url, BITLY_TOKEN):
     return response.ok
 
 
-if __name__ == '__main__':
+def main():
+    env = Env()
+    env.read_env()
+    BITLY_TOKEN = env("BITLY_TOKEN")
     parser = argparse.ArgumentParser()
-    url_arg = parser.add_argument('url')
+    parser.add_argument('url')
     args = parser.parse_args()
-    link_from_args = args.url
-    url_parts = urlparse(link_from_args)
+    url_parts = urlparse(args.url)
     basic_url = f'{url_parts.netloc}{url_parts.path}'
     if is_bitlink(basic_url, BITLY_TOKEN):
         print('Total clicks:', count_clicks(BITLY_TOKEN, basic_url))
     else:
-        print('Shorten link:', shorten_link(BITLY_TOKEN, link_from_args))
+        print('Shorten link:', shorten_link(BITLY_TOKEN, args.url))
+
+
+if __name__ == '__main__':
+    main()
+
